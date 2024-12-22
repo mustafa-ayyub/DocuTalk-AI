225ad7c067dbd65a6c04235fedfeaa2c25843c1f
Wajeeha Abdullah <wajeehaabdullah236@gmail.com>
2024-12-22 18:00:51 +0100

    Hugging face Integration

--- src/services/embeddings.py
+++ src/services/embeddings.py
@@ -8,19 +8,57 @@ from langchain_community.vectorstores import FAISS
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def get_vectorstore(text_chunks):
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+Hugging_face_api_key=os.getenv("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")
+
+
+
+
+def get_vectorstore(text_chunks, use_huggingface=True):
     """
-    Creates a FAISS vectorstore from text chunks.
+    Creates a Chroma vectorstore from text chunks, using either OpenAI or Hugging Face embeddings.
 
     Args:
-        text_chunks (list): List of text chunks to embed.
+        text_chunks (list[str]): List of text chunks to embed.
+        use_huggingface (bool): If True, use Hugging Face embeddings. Otherwise, use OpenAI.
 
     Returns:
-        vectorstore (FAISS): The created FAISS vectorstore.
+        vectorstore (Chroma): The created Chroma vectorstore.
     """
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    # Currently, we are using FAISS, but in the future, we can consider using alternatives
-    # such as Pinecone or Weaviate if the dataset grows larger
+
+    if use_huggingface:
+        # Example: using the MiniLM-L6-v2 model from sentence-transformers
+        embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+        print("Using Hugging Face Embeddings...")
+    else:
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        print("No Api KEY...")
+
+    # ChromaDb(Light Weight FIASS ALTERNATIVE)
+    # vectorstore = Chroma.from_texts(text_chunks, embeddings)
+    # print("Chroma Vectorstore created:", vectorstore)
+
+    # return vectorstore
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     print("Vectorstore is: ", vectorstore)
     return vectorstore
+
+# def get_vectorstore(text_chunks):
+#     """
+#     Creates a FAISS vectorstore from text chunks.
+
+#     Args:
+#         text_chunks (list): List of text chunks to embed.
+
+#     Returns:
+#         vectorstore (FAISS): The created FAISS vectorstore.
+#     """
+#     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    
+#     # Currently, we are using FAISS, but in the future, we can consider using alternatives
+#     # such as Pinecone or Weaviate if the dataset grows larger
+#     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+#     print("Vectorstore is: ", vectorstore)
+#     return vectorstore

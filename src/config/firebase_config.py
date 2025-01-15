6d7932882d6bfadcafcf3f91e1d2f125e64bdc5d
Mustafa Ayyub <mustafaayyub.dev@gmail.com>
2025-01-15 17:34:31 +0100

    Update the firebase configs

--- src/config/firebase_config.py
+++ src/config/firebase_config.py
@@ -20,10 +20,20 @@ firebase_config = {
     "appId": os.getenv("FIREBASE_APP_ID"),
 }
 
-service_account_path = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(service_account_path)
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    })
     firebase_admin.initialize_app(
         cred, {"databaseURL": os.getenv("FIREBASE_DATABASE_URL")}
     )
